a0ba37c51a5ff9366d7cdc753cf35c8944cedf91
Owendrinkwater <owenowade@gmail.com>
2026-04-08 01:27:40 +0300

    Complete loops and arrays exercises
    Implement solutions for repeatString, reverseString,
    removeFromArray, sumAll, leapYears, and tempConversion
    
    Add input validation where required and ensure all
    functions pass the provided test cases

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (
+        typeof a !== "number" ||
+        typeof b !== "number" ||
+        !Number.isInteger(a) ||
+        !Number.isInteger(b) ||
+        a < 0 ||
+        b < 0
+      ) {
+        return "ERROR";
+    }
+
+    const min = Math.min(a, b);
+    const max = Math.max(a, b);
+
+    let sum = 0;
+
+    for(let i = min; i <= max; i++){
+        sum += i;
+    }
+
+    return sum;
 
 };
 
